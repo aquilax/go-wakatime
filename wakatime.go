7aa799c40d2a9c0ffe5731d6c4bd38c3afc4dd9c
aquilax <aquilax@gmail.com>
2015-04-23 12:16:01 +0200

    Use transport

--- wakatime.go
+++ wakatime.go
@@ -1,14 +1,37 @@
 package wakatime
 
+import (
+	"net/http"
+)
+
+const ApiBase = "http://api"
+
 type WakaTime struct {
-	APIKey string
+	client *http.Client
 }
 
-func NewWakaTime(APIKey string) *WakaTime {
-	return &WakaTime{APIKey}
+func NewWakaTime(tr *http.Transport) *WakaTime {
+	return &WakaTime{
+		client: &http.Client{
+			Transport: tr,
+		},
+	}
 }
 
-func (wt *WakaTime) Durations() {}
-func (wt *WakaTime) Stats()     {}
+func (wt *WakaTime) Durations() {
+	_, _ = wt.fetch("/durations")
+}
+
+func (wt *WakaTime) Stats() {}
+
 func (wt *WakaTime) Summaries() {}
-func (wt *WakaTime) Users()     {}
+
+func (wt *WakaTime) Users() {}
+
+func (wt *WakaTime) fetch(path string) (*http.Response, error) {
+	return wt.client.Get(wt.getURL(path))
+}
+
+func (wt *WakaTime) getURL(path string) string {
+	return ApiBase + path
+}
